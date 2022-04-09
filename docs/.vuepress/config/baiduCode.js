e840cd6210eaeda68e3a1b5ef30c0ef9ec02795d
timchen525 <461330266@qq.com>
2022-04-09 09:54:43 +0800

    docs：提交百度统计的代码

--- docs/.vuepress/config/baiduCode.js
+++ docs/.vuepress/config/baiduCode.js
@@ -1,1 +1,11 @@
-module.exports = '';
+module.exports =  {
+                     head:[[ 'script', {}, `
+                     var _hmt = _hmt || [];
+                     (function() {
+                       var hm = document.createElement("script");
+                       hm.src = "https://hm.baidu.com/hm.js?256b0f411e8441cbb78d9124bf7f926a";
+                       var s = document.getElementsByTagName("script")[0];
+                       s.parentNode.insertBefore(hm, s);
+                     })();
+                   ]],
+                   }
