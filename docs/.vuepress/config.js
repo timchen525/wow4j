43b810d55e2170c5560a58b48f0b97c683768155
chenjintian <461330266@qq.com>
2022-12-07 14:49:16 +0800

    docs:添加临时笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,13 +9,12 @@ module.exports = {
   // theme: require.resolve('../../vdoing'), // 使用本地主题
 
   title: "Wow4j",
-  description: '一个神奇的 Java 网站',
+  description: '一个神奇的个人网站',
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
-
     ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
     ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
   ],
@@ -25,31 +24,45 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       {
-        text: '指南', link: '/pages/a2f161/', items: [
-          { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
-          { text: '介绍', link: '/pages/a2f161/' },
-          { text: '快速上手', link: '/pages/793dcb/' },
-          { text: '目录结构', link: '/pages/2f674a/' },
-          { text: '核心配置和约定', link: '/pages/33d574/' },
-          { text: '自动生成front matter', link: '/pages/088c16/' },
-          { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
-          { text: 'Markdown 中使用组件', link: '/pages/197691/' },
-          {
-            text: '相关文章', items: [
-              { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
-              { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
-              { text: '部署', link: '/pages/0fc1d2/' },
-              { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
-              { text: '关于博客搭建与管理', link: '/pages/26997d/' },
-              { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
-            ]
-          }
-        ]
-      }
+        text: '个人简介', link: '/pages/f380dd/'
+      },
+      {
+        text: '后端开发', link: '/pages/19aee3/'
+      },
+      {
+        text: '前端开发', link: '/pages/d00ad7/'
+      },
+      {
+        text: '测试开发', link: '/pages/ca725f/'
+      },
+      {
+        text: '运维开发', link: '/pages/df4369/'
+      },
+      {
+        text: '大数据开发', link: '/pages/aa09ae/'
+      },
+      {
+        text: '产品经理', link: '/pages/b9db3b/'
+      },
+      {
+        text: 'UI 交互', link: '/pages/5b4bcd/'
+      },
+      {
+        text: '团队管理', link: '/pages/7ba30c/'
+      },
+      {
+        text: '软技能', link: '/pages/92a016/'
+      },
+      {
+        text: '开源产品', link: '/pages/366b2d/'
+      },
+      {
+        text: '敬请期待', link: '/pages/fdeb84/'
+      },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200409124835.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: '/timchen525/wow4j', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
@@ -86,8 +99,8 @@ module.exports = {
     // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
 
     author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-      name: 'Evan Xu', // 必需
-      href: 'https://github.com/xugaoyi' // 可选的
+      name: 'timchen525', // 必需
+      href: 'https://github.com/timchen525/wow4j' // 可选的
     },
     social: { // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
@@ -95,12 +108,12 @@ module.exports = {
         {
           iconClass: 'icon-youjian',
           title: '发邮件',
-          link: 'mailto:894072666@qq.com'
+          link: 'mailto:461330266@qq.com'
         },
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/xugaoyi'
+          link: 'https://github.com/timchen525'
         },
         {
           iconClass: 'icon-erji',
@@ -110,8 +123,8 @@ module.exports = {
       ]
     },
     footer: { // 页脚信息
-      createYear: 2019, // 博客创建年份
-      copyrightInfo: 'Evan Xu | MIT License', // 博客版权信息，支持a标签
+      createYear: 2022, // 博客创建年份
+      copyrightInfo: 'timchen525 | MIT License', // 博客版权信息，支持a标签
     },
     htmlModules,
   },
